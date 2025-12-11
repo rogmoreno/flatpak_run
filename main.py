3c787940ee85617e0e0a62ab2766e692de1330e9
rogmoreno <rogmoreno@gmail.com>
2025-12-11 15:51:21 -0500

    Fix: Use self.lm.get() for localization instead of gettext._() in main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 # main.py
-from gettext import gettext as _
 from src.backend.PluginManager.PluginBase import PluginBase
 from src.backend.PluginManager.ActionHolder import ActionHolder
 from .actions.run_flatpak.run_flatpak import RunFlatpak
@@ -8,18 +7,20 @@ class FlatpakRunPlugin(PluginBase):
     def __init__(self):
         super().__init__()
 
+        self.lm = self.locale_manager
+
         # --- Register Action ---
         self.run_flatpak_action_holder = ActionHolder(
             plugin_base=self,
             action_base=RunFlatpak,
             action_id="com.rogmoreno.flatpak_run.run",
-            action_name=_("actions.flatpak.name"),
+            action_name=self.lm.get("actions.flatpak.name"),
         )
         self.add_action_holder(self.run_flatpak_action_holder)
 
         # --- Register Plugin ---
         self.register(
-            plugin_name=_("plugin.name"),
+            plugin_name=self.lm.get("plugin.name"),
             github_repo="https://github.com/rogmoreno/flatpak_run.git",
             plugin_version="1.0.0",
             app_version="1.1.1-alpha",
